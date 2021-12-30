292c5f67b8e7e67ef90a77ed4ffde2b828137369
Karol Kulesza <kar.kul1993@gmail.com>
2021-12-30 20:30:09 +0100

    no message

---
+++ SimulatorApp/Application/InputHandler.cs
@@ -0,0 +1,23 @@
+﻿using SFML.Window;
+
+namespace SimulatorApp.Application;
+
+public class InputHandler
+{
+    private readonly CellularAutomata automata;
+
+    public InputHandler(CellularAutomata automata)
+    {
+        this.automata = automata;
+    }
+
+    public void HandleMouseClick(Mouse.Button button)
+    {
+
+    }
+
+    public void HandleKeyboardPress(Keyboard.Key key)
+    {
+
+    }
+}
---
+++ SimulatorApp/Common/Enums/CellType.cs
@@ -0,0 +1,21 @@
+﻿namespace SimulatorApp.Common.Enums;
+
+public enum CellType
+{
+    Air,
+    // Dynamic solids
+    Sand,
+    Ash,
+    // Static solids
+    Rock,
+    Wood,
+    Leaves,
+    // Liquids
+    Water,
+    // Gases
+    Steam,
+    Smoke,
+    // Other
+    Fire
+}
+
